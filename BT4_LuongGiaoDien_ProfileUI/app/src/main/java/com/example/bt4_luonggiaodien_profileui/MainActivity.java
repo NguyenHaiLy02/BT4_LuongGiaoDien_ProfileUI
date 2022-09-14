f8a858b1235e34c61f09ada77e1f192b40c4d21c
NguyenHaiLy02 <nguyenhaily1014@gmail.com>
2022-09-14 10:04:20 +0700

    2nd commit

--- BT4_LuongGiaoDien_ProfileUI/app/src/main/java/com/example/bt4_luonggiaodien_profileui/MainActivity.java
+++ BT4_LuongGiaoDien_ProfileUI/app/src/main/java/com/example/bt4_luonggiaodien_profileui/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.bt4_luonggiaodien_profileui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    EditText edtEmail,edtPass;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmail = (EditText) findViewById(R.id.editText_email);
+        edtPass = (EditText) findViewById(R.id.editText_pass);
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email="admin";
+                String pass ="123456";
+                if(edtEmail.getText().toString().equals(email) && edtPass.getText().toString().equals(pass)){
+                    Toast.makeText(MainActivity.this, "ĐĂNG NHẬP THÀNH CÔNG", Toast.LENGTH_SHORT).show();
+                    Intent myIntent=new Intent(MainActivity.this, ProfileActivity.class);
+                    startActivity(myIntent);
+                }else {
+                    Toast.makeText(MainActivity.this, "ĐĂNG NHẬP THẤT BẠI", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
